9c64ba7cd54dee16be129f375ec3a6cfbb374de7
Cole Bittel <cole.bittel@gmail.com>
2018-12-07 15:21:06 -0800

    Tree created

--- advent-of-code-2018/7/main.go
+++ advent-of-code-2018/7/main.go
@@ -5,12 +5,29 @@ import (
 	"log"
 )
 
-type depMap struct {
-	Deps []string
-	Node string
-}
+type DepTree map[string][]string
 
 func main() {
-	depSimple := text.DefineData()
-	log.Println("depSimple:\n", depSimple)
+	depsSimple := text.DefineData()
+	tree := make(map[string][]string)
+	//for _, singleDep := range (*depsSimple)[:9] {
+	for _, singleDep := range *depsSimple {
+		tree[singleDep.Node] = append(tree[singleDep.Node], singleDep.Dep)
+		if len(tree[singleDep.Dep]) == 0 {
+			tree[singleDep.Dep] = make([]string, 0)
+		}
+	}
+
+	for v, k := range tree {
+		log.Printf("%v:%v\n", v, k)
+	}
+
+	for n := 1; n < 27; n++ {
+		log.Println("iteration = ", n)
+		for node, deps := range tree {
+			if len(deps) == 0 {
+				log.Println("no deps:", node)
+			}
+		}
+	}
 }
