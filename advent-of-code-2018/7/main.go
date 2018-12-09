ba20d6191bed71f1825524719643ef0a4e467c5c
Cole Bittel <cole.bittel@gmail.com>
2018-12-08 17:35:10 -0800

    need to log candidates first before removing from deps

--- advent-of-code-2018/7/main.go
+++ advent-of-code-2018/7/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/bittelc/advent-of-code-2018/7/text"
 	"log"
 )
@@ -22,12 +23,57 @@ func main() {
 		log.Printf("%v:%v\n", v, k)
 	}
 
-	for n := 1; n < 27; n++ {
-		log.Println("iteration = ", n)
+	solutionString := ""
+	var candidates []string
+FINDER:
+	for {
+		candidates = []string{}
 		for node, deps := range tree {
 			if len(deps) == 0 {
-				log.Println("no deps:", node)
+				candidates = append(candidates, node)
+				log.Println("node, candidates:", node, candidates)
 			}
 		}
+		solutionString = solutionString + fmt.Sprintf("%v", candidates) + "|"
+			
+		for node, deps := range tree {
+				for _, candidate := range candidates {
+					if contains(deps, candidate) {
+						log.Printf("%v has dependency on %v from its list: %v", , node, depsWithNode)
+						i := indexOf(node, depsWithNode)
+						log.Println("i =", i)
+						depsWithNode := append(depsWithNode[:i], depsWithNode[i+1:]...)
+						tree[nodeWithNode] = depsWithNode
+						log.Printf("%v doesn't have dep on %v list:        %v", nodeWithNode, node, depsWithNode)
+					}
+				}
+			}
+		}
+
+		if len(solutionString) > 60 {
+			for v, k := range tree {
+				log.Printf("%v:%v\n", v, k)
+			}
+			break FINDER
+		}
 	}
+	log.Println(solutionString)
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func indexOf(word string, data []string) int {
+	for k, v := range data {
+		if word == v {
+			return k
+		}
+	}
+	return -1
 }
