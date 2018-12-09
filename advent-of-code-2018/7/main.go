77dac751a10d5b7e8124ec03dfee94fd696530b0
Cole Bittel <cole.bittel@gmail.com>
2018-12-09 13:55:46 -0800

    Working clean part a

--- advent-of-code-2018/7/main.go
+++ advent-of-code-2018/7/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"github.com/bittelc/advent-of-code-2018/7/text"
 	"log"
+	"sort"
 )
 
 type DepTree map[string][]string
@@ -11,7 +12,6 @@ type DepTree map[string][]string
 func main() {
 	depsSimple := text.DefineData()
 	tree := make(map[string][]string)
-	//for _, singleDep := range (*depsSimple)[:9] {
 	for _, singleDep := range *depsSimple {
 		tree[singleDep.Node] = append(tree[singleDep.Node], singleDep.Dep)
 		if len(tree[singleDep.Dep]) == 0 {
@@ -19,44 +19,27 @@ func main() {
 		}
 	}
 
-	for v, k := range tree {
-		log.Printf("%v:%v\n", v, k)
-	}
-
 	solutionString := ""
 	var candidates []string
-FINDER:
 	for {
 		candidates = []string{}
-		for node, deps := range tree {
-			if len(deps) == 0 {
-				candidates = append(candidates, node)
-				log.Println("node, candidates:", node, candidates)
-				delete(tree, node)
+		for nodeA, _ := range tree {
+			if len(tree[nodeA]) == 0 {
+				candidates = append(candidates, nodeA)
 			}
 		}
-		solutionString = solutionString + fmt.Sprintf("%v", candidates) + "|"
-
-		for node, deps := range tree {
-			for _, candidate := range candidates {
-				if contains(deps, candidate) {
-					log.Printf("%v has dependency on %v from its list: %v", node, candidate, tree[node])
-					i := indexOf(candidate, deps)
-					log.Println("i =", i)
-					log.Println("deps preremoval=  ", deps)
-					deps := remove(deps, i)
-					log.Println("deps postremoval= ", deps)
-					tree[node] = deps
-					log.Printf("%v doesn't have dep on %v list:        %v", node, candidate, tree[node])
-				}
-			}
+		sort.Strings(candidates)
+		if len(candidates) == 0 {
+			break
 		}
+		delete(tree, candidates[0])
+		solutionString = solutionString + candidates[0]
 
-		if len(solutionString) > 100 {
-			for v, k := range tree {
-				log.Printf("%v:%v\n", v, k)
+		for nodeB, _ := range tree {
+			if contains(tree[nodeB], candidates[0]) {
+				i := indexOf(candidates[0], tree[nodeB])
+				tree[nodeB] = remove(tree[nodeB], i)
 			}
-			break FINDER
 		}
 	}
 	log.Println(solutionString)
