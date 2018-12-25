99cca6291c190050ed8fdcb396a7ad5f80975d20
Cole Bittel <cole.bittel@gmail.com>
2018-12-24 21:55:36 -0600

    WIP input parsing

--- 24/main.go
+++ 24/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 type Group struct {
 	NumUnits     int
@@ -17,6 +23,42 @@ type ImmuneSystem struct {
 	Groups []Group
 }
 
+var is ImmuneSystem
+var inf Infection
+
 func main() {
-	fmt.Println("vim-go")
+	err := structify("example_text.txt")
+	if err != nil {
+		log.Fatalln("err:", err)
+	}
+}
+
+func structify(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	fileScanner := bufio.NewScanner(f)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		fmt.Println("line:", line)
+		err := makeArmy(line)
+		if err != nil {
+			return err
+		}
+	}
+	log.Println("is:", is)
+	return nil
+}
+
+func makeArmy(sentence string) error {
+	unitIndex := strings.Index(sentence, " ")
+	if unitIndex <= 0 {
+		return nil
+	}
+	is.Groups[0].NumUnits = unitIndex
+	log.Println("is:", is)
+	return nil
 }
