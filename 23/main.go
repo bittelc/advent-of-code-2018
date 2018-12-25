73cded43693c0383228001c5231d1add5d6dfbec
Cole Bittel <cole.bittel@gmail.com>
2018-12-25 00:02:36 -0600

    complete part 1 of 23

--- 23/main.go
+++ 23/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type Chars struct {
 	X         int
@@ -10,6 +17,65 @@ type Chars struct {
 	CloseBots int
 }
 
+var filename = "input.txt"
+var bots = make(map[int]*Chars)
+
+var botWithBigRadius = 0
+
 func main() {
-	fmt.Println("vim-go")
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln("err:", err)
+	}
+	defer f.Close()
+	fileScanner := bufio.NewScanner(f)
+
+	for i := 0; fileScanner.Scan(); i++ {
+		line := fileScanner.Text()
+		bots[i] = structify(line)
+		if bots[i].R > bots[botWithBigRadius].R {
+			botWithBigRadius = i
+		}
+	}
+	updateCloseBots(botWithBigRadius)
+	log.Println("Bots within biggest radius:", bots[botWithBigRadius].CloseBots)
+}
+
+func structify(line string) *Chars {
+	chars := &Chars{CloseBots: 0}
+	var err error
+	comp := strings.Split(line, ",")
+	x := strings.TrimPrefix(comp[0], "pos=<")
+	chars.X, err = strconv.Atoi(x)
+	if err != nil {
+		log.Fatalln("err:", err)
+	}
+	chars.Y, err = strconv.Atoi(comp[1])
+	if err != nil {
+		log.Fatalln("err:", err)
+	}
+	z := strings.Trim(comp[2], ">")
+	chars.Z, err = strconv.Atoi(z)
+	if err != nil {
+		log.Fatalln("err:", err)
+	}
+	r := strings.TrimPrefix(comp[3], " r=")
+	chars.R, err = strconv.Atoi(r)
+	if err != nil {
+		log.Fatalln("err:", err)
+	}
+	return chars
+}
+
+func updateCloseBots(a int) error {
+	for b := 0; b < len(bots); b++ {
+		xDistance := math.Abs(float64(bots[a].X - bots[b].X))
+		yDistance := math.Abs(float64(bots[a].Y - bots[b].Y))
+		zDistance := math.Abs(float64(bots[a].Z - bots[b].Z))
+		total := xDistance + yDistance + zDistance
+		if int(total) <= bots[a].R {
+			bots[a].CloseBots++
+		}
+	}
+	return nil
 }
